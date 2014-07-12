f4f0abb2e263486e0439bd3eed39c38a8db5ac2b
Boloop <boloop@gmail.com>
2014-07-13 00:10:36 +0100

    Improve DB schema

--- web/DataEngine.py
+++ web/DataEngine.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import sessionmaker
 
 #Base Class Generated
@@ -17,17 +18,52 @@ class User(Base):
 	def __repr__(self):
 		return "<User(name="+self.name+")>"
 
+class Project(Base):
+	__tablename__ = "Projects"
+	id = Column(Integer, primary_key=True)
+	dateAddedUTC = Column(DateTime)
+	projectOpen = Column(Boolean)
+	projectHidden = Column(Boolean)
+
+	shortName = Column(String)
+	longName = Column(String)
+
+class Task(Base):
+	__tablename__ = "Tasks"
+	id = Column(Integer, primary_key=True)
+	parentProject = Column(Integer, ForeignKey('Projects.id'))
+	dateAddedUTC = Column(DateTime)
+	priority = Column(Integer)
+	shortName = Column(String)
+	createdBy = Column(Integer, ForeignKey('Users.id'))
+	assignedTo = Column(Integer, ForeignKey('Users.id'))
+	isCompleted = Column(Boolean)
+	dateCompleted = Column(DateTime)
+
+
+class DB():
+	def __init__(self):
+		self.engine = None
+		self.Session = None
+	def Ignition(self):
+		engine = create_engine('sqlite:///:memory:', echo=True)	
+		Base.metadata.create_all(engine)  # Create all tables...
+		self.Session = sessionmaker(bind=engine)
+	def GetSession(self):
+		return self.Session()
+
 if __name__ == "__main__":
 	print User.__table__
 
 	one_user = User(name="one")
 	print one_user
 	#The engine
-	engine = create_engine('sqlite:///:memory:', echo=True)
-	Base.metadata.create_all(engine)  # Create all tables...
-	Session = sessionmaker(bind=engine)
+	db = DB()
 
-	ses = Session()
+	db.Ignition()
+
+
+	ses = db.GetSession()
 
 	ses.add(one_user)
 
--- web/web.py
+++ web/web.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from sqlalchemy import create_engine
+
+import DataEngine
+
 app = Flask(__name__)
+
+db = DataEngine.DB()
+db.Ignition()
+
 
 @app.route("/")
 def hello():
-    return "ProjectTask is running!"
+	result = "Running"
+	ses = db.GetSession()
+	return result
 
 if __name__ == "__main__":
     app.run()
